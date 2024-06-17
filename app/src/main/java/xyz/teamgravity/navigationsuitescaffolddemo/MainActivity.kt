ba35c2f6ec143c6fc75c795d9a8bee5b5eab445b
Raheem <raheemideadev@gmail.com>
2024-06-17 21:49:57 +0500

    Setup Material3;

--- app/src/main/java/xyz/teamgravity/navigationsuitescaffolddemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/navigationsuitescaffolddemo/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import xyz.teamgravity.navigationsuitescaffolddemo.ui.theme.NavigationSuiteScaffoldDemoTheme
 
 class MainActivity : ComponentActivity() {
